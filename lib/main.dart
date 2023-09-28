0754364b294d498789d3391951f5e509fab05a7d
DavideTicchiarelli <s1098361@studenti.univpm.it>
2023-09-28 20:36:36 +0200

    app solo in verticale

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'Views/Login.dart';
 import 'Views/Signup.dart';
 
 
 Future<void> main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const MyApp());
+  });
+
+
 }
 
 class MyApp extends StatelessWidget {
