7ed6f0764ad642ff749ceb95e7c84181f97bcc47
DavideTicchiarelli <s1098361@studenti.univpm.it>
2023-10-31 17:31:55 +0100

    layout modifica password

--- lib/Views/ModificaPassword.dart
+++ lib/Views/ModificaPassword.dart
@@ -14,40 +14,135 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Reset Password'),
+      body: Container(
+        color: const Color(0xFF00BFFF),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 44.0, left: 15.0),
+                    child: FloatingActionButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      backgroundColor: Colors.white,
+                      child: const Icon(
+                        Icons.arrow_back_ios_new,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  const Center(
+                    child: Icon(
+                      Icons.account_circle,
+                      size: 150.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Center(
+                    child: Text(
+                      "Reset Password",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 42.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  const SizedBox(height: 16.0),
+                  Material(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(48),
+                        topRight: Radius.circular(48),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          const Text(
+                                "Inserisci la tua Email per ricevere la mail di modifica",
+                                style: TextStyle(
+                                  color: Color(0xFF00BFFF),
+                                  fontSize: 10.0,
+                                ),
+                              ),
+
+                          createTextField(
+                            controller: emailController,
+                            labelText: "Email",
+                            prefixIcon: Icons.mail,
+                          ),
+                          const SizedBox(height: 15.0),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Implement the reset password functionality here
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFF00BFFF),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                minimumSize: const Size(
+                                  double.infinity,
+                                  50.0,
+                                ),
+                              ),
+                              child: const Text(
+                                "Reset Password",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16.0),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                userViewModel.sendPasswordResetEmail(
-                  emailController.text,
-                      () {
-                    Fluttertoast.showToast(
-                      msg: 'Controlla la tua Email',
-                      toastLength: Toast.LENGTH_SHORT,
-                    );
-                  },
-                      () {
-                    Fluttertoast.showToast(
-                      msg: 'La Email inserita non è corretta',
-                      toastLength: Toast.LENGTH_SHORT,
-                    );
-                  },
-                );
-              },
-              child: Text('Reset Password'),
-            ),
-          ],
+    );
+  }
+
+  Widget createTextField({
+    TextEditingController? controller,
+    String labelText = "",
+    IconData? prefixIcon,
+    double height = 50.0,
+    bool obscureText = false,
+  }) {
+    return Container(
+      height: height,
+      margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.0),
+        color: const Color(0x1A00bfff),
+      ),
+      child: TextFormField(
+        controller: controller,
+        obscureText: obscureText, // Use the provided obscureText option
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          labelText: labelText,
+          prefixIcon: prefixIcon != null
+              ? Icon(prefixIcon, color: Color(0xFF00BFFF))
+              : null,
         ),
       ),
     );
